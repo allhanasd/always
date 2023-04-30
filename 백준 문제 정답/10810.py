cbf77b57cc311804ce38390e6ec90fd3bf7e36a5
allhanasd <132112933+allhanasd@users.noreply.github.com>
2023-04-30 02:38:08 +0000

    10810

---
+++ 백준 문제 정답/10810.py
@@ -0,0 +1,10 @@
+n, m = list(map(int, input().split()))
+l = []
+for i in range(n+1):
+    l.append(0)
+    
+for p in range(m):
+    i, j, k = list(map(int, input().split()))
+for idx in range(j, k):
+    l[p]
+print()
